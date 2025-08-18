35bd828913a9aa02098737c65118638196cedcac
dheysson <dheyssonsousasilva@gmail.com>
2025-08-17 20:41:54 -0400

    google verification line

--- app/layout.js
+++ app/layout.js
@@ -1,39 +1,51 @@
-
 import "./globals.css";
 
 //IMPORT HEADER
-import Header from "@/components/header"
+import Header from "@/components/header";
 //IMPORT FOOTER
 import Footer from "@/components/footer";
 
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 //FONT IMPORT
-import localFont from 'next/font/local';
+import localFont from "next/font/local";
 const archivo = localFont({
-    src: [{
-        path: '../public/fonts/Archivo-Regular.ttf',
-        weight: '400'
-    }],
-    variable: '--font-archivo'
-})
+  src: [
+    {
+      path: "../public/fonts/Archivo-Regular.ttf",
+      weight: "400",
+    },
+  ],
+  variable: "--font-archivo",
+});
 
 export const metadata = {
   title: "Schrijnwerkerij de haes",
-  description: "Algemene binnen en buitenschrijnwerkerij - Daktimmers, dakbedekking, gevelbekleding en terassen",
+  description:
+    "Algemene binnen en buitenschrijnwerkerij - Daktimmers, dakbedekking, gevelbekleding en terassen",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="nl" className="scroll-smooth">
       <head>
-        <meta name="keywords" content="schrijnwerkerij, de haes, sd schrijnwerkerij, schrijnwerkerij mb, sbk schrijnwerkerij, Belgique, carpentry"></meta>
+        <meta
+          name="google-site-verification"
+          content="BjIFUYGdtB0SCZnPm292PJEtnbnf9ndl3D-zqgsm0BA"
+        />
+        <meta
+          name="keywords"
+          content="schrijnwerkerij, de haes, sd schrijnwerkerij, schrijnwerkerij mb, sbk schrijnwerkerij, Belgique, carpentry"
+        ></meta>
       </head>
-      <meta name="google-site-verification" content="iJG9g6Q9U-v746a0tzQHDktkoMmmpHSIrWBU9EAjGus" />
+      <meta
+        name="google-site-verification"
+        content="iJG9g6Q9U-v746a0tzQHDktkoMmmpHSIrWBU9EAjGus"
+      />
       <body className={`${archivo.variable} antialiased`}>
         <Header></Header>
         {children}
-        <Analytics/>
+        <Analytics />
         <Footer></Footer>
       </body>
     </html>
