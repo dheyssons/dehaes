42aaa5fc790c3a64ab5d489a2883caa672de66d1
dheysson <dheyssonsousasilva@gmail.com>
2025-08-19 19:23:52 -0400

    optimize css in `next.config.mjs` file

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        PUBLICKEY: 'TZPoFP8sOe4vVKffU',
-        SERVICEID: 'service_ys5fnbq',
-        TEMPLATEID: 'template_dyrghy7'
-    }
+  env: {
+    PUBLICKEY: "TZPoFP8sOe4vVKffU",
+    SERVICEID: "service_ys5fnbq",
+    TEMPLATEID: "template_dyrghy7",
+  },
+  experimental: {
+    optimizeCss: true,
+  },
 };
 
 export default nextConfig;
