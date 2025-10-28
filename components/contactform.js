a03377b646af5f848f400efb6f2f107abff785e6
dheysson <dheyssonsousasilva@gmail.com>
2025-10-28 12:28:18 -0400

    change

--- components/contactform.js
+++ components/contactform.js
@@ -163,7 +163,7 @@ export default function ContactForm() {
                     htmlFor="subject"
                     className="peer-focus:font-medium absolute text-sm text-gray-700 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   >
-                    Objet
+                    Sujet
                   </label>
                 </motion.div>
                 <motion.div variants={item} className="grid md:gap-6 w-full">
