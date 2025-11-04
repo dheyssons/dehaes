997a325163f70bb2467ee026bbd84ee09876c9c9
dheysson <dheyssonsousasilva@gmail.com>
2025-11-03 20:58:13 -0400

    meta pixel integration test 2

--- app/layout.js
+++ app/layout.js
@@ -27,6 +27,7 @@ export const metadata = {
 };
 
 import "./globals.css";
+import MetaPixel from "@/components/MetaPixel";
 
 export default function RootLayout({ children }) {
   return (
@@ -64,15 +65,15 @@ export default function RootLayout({ children }) {
           dangerouslySetInnerHTML={{
             __html: `
             !function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1406701764156938');
-fbq('track', 'PageView');
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1406701764156938');
+            fbq('track', 'PageView');
             `,
           }}
         ></script>
@@ -93,6 +94,7 @@ fbq('track', 'PageView');
         {children}
         <CookieConsent />
 
+        <MetaPixel />
         <Analytics />
         <Footer></Footer>
       </body>
