3156bd5259b1d02bb26e21e3aefa6fd348fa6b28
dheysson <dheyssonsousasilva@gmail.com>
2026-01-23 13:44:26 -0400

    fix imediately

--- components/marquee.js
+++ components/marquee.js
@@ -9,12 +9,32 @@ import Derbigum from "@/public/images/brands/derbigum.webp";
 
 export default function Brands() {
   return (
-    <Marquee className="my-44 w-full">
-      <Image className="w-28 mx-10 md:mx-24" src={Velux} alt="velux" />
-      <Image className="w-28 mx-10 md:mx-24" src={Bosscover} alt="bosscover" />
-      <Image className="w-28 mx-10 md:mx-24" src={Resitrix} alt="resitrix" />
-      <Image className="w-28 mx-10 md:mx-24" src={Iko} alt="iko" />
-      <Image className="w-28 mx-10 md:mx-24" src={Derbigum} alt="derbigum" />
+    <Marquee className="my-8 md:my-24 w-full">
+      <Image
+        className="w-28 mx-10 md:mx-24 object-contain"
+        src={Velux}
+        alt="velux"
+      />
+      <Image
+        className="w-28 mx-10 md:mx-24 object-contain"
+        src={Bosscover}
+        alt="bosscover"
+      />
+      <Image
+        className="w-28 mx-10 md:mx-24 object-contain"
+        src={Resitrix}
+        alt="resitrix"
+      />
+      <Image
+        className="w-28 mx-10 md:mx-24 object-contain"
+        src={Iko}
+        alt="iko"
+      />
+      <Image
+        className="w-28 mx-10 md:mx-24 object-contain"
+        src={Derbigum}
+        alt="derbigum"
+      />
     </Marquee>
   );
 }
